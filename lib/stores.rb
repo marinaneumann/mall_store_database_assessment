fa1cfe567cdcdf066beb0ff5afbb7be3f7395336
Marina Neumann <marinaneumann2468@yahoo.com>
2014-08-19 11:07:04 -0700

    Add stores to system and save them, can compare two different stores and recognize their same attributes.

--- lib/stores.rb
+++ lib/stores.rb
@@ -8,6 +8,22 @@ end
 
 def self.all
 	@stores = []
+	results = DB.exec("SELECT * FROM stores;")
+	results.each do |result|
+		@stores << Store.new({:name =>result['name']})
+	end
+	@stores
 end
+
+def save
+	results = DB.exec("INSERT INTO stores (name) VALUES ('#{@name}');")
+end
+
+def ==(another_store)
+	self.name == another_store.name
+end
+
+
+
 
 end
--- spec/stores_spec.rb
+++ spec/stores_spec.rb
@@ -10,4 +10,16 @@ describe Store do
 	it 'starts with an empty array of stores' do
 		expect(Store.all).to eq []
 	end
+
+	it 'adds a store and saves it' do 
+		test_store = Store.new({:name => 'Nike'})
+		test_store.save
+		expect(Store.all).to eq [test_store]
+	end
+
+	it 'compares two stores with same attributes' do 
+		test_store = Store.new({:name => 'Nike'})
+		test_store1 = Store.new({:name => 'Nike'})
+		expect(test_store).to eq test_store1
+	end
 end
