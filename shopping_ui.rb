88cc0325ad00b76f771942ae67a208984a6d3f1d
Marina Neumann <marinaneumann2468@yahoo.com>
2014-08-19 11:51:52 -0700

    Add malls method in user interface plus addition of a mall_menu.

--- shopping_ui.rb
+++ shopping_ui.rb
@@ -26,4 +26,39 @@ def main_menu
 	end
 end
 
+def mall_menu
+	puts "**Exploring Malls**"
+	puts "[1] to add a mall"
+	puts "[2] to update mall registry"
+	puts "[3] to add stores to a mall"
+	puts "[4] to search for a mall"
+	puts "[5] to go back to main_menu"
+
+	choice = gets.chomp
+
+	if choice == '1'
+		add_mall
+	elsif choice == '2'
+		update_mall
+	elsif choice == '3'
+		add_store_mall
+	elsif choice == '4'
+		search_mall
+	elsif choice == '5'
+		main_menu
+	else
+		puts "Please put a valid option"
+	end
+end
+
+def add_mall 
+	puts "What mall would you like to add?"
+	mall_input = gets.chomp
+	new_mall = Mall.new({:name => mall_input})
+	new_mall.save
+	puts "#{Mall.all.last.name} added to the directory"
+end
+
+
+end
 main_menu
